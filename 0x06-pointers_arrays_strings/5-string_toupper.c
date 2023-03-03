4511c3a8a0a2223e664eb3e0d2a3e89ce81aa995
Your-Full-Name <your-email-address>
2023-03-03 20:51:45 +0200

    3

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - convernt lower case to upper case
+ * @str: string to be converted
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+return (str);
+}
