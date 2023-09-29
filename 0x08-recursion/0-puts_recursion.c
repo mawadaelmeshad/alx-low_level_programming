30a1c3712055c18658986f0a36077f39cd2089e9
Mawadah Elmashad <mawadahelmashad@std.mans.edu.eg>
2023-09-29 09:56:39 +0300

    d

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-    /* Base case: If the current character is the null terminator, print a new line and return. */
-    if (*s == '\0')
-    {
-        putchar('\n');
-        return;
-    }
+	/* Base case: If the current character is the null terminator, print a new line and return. */
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
 
-    /* Print the current character and recursively call _puts_recursion with the next character. */
-    putchar(*s);
-    _puts_recursion(s + 1);
+	/* Print the current character and recursively call _puts_recursion with the next character. */
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
-
